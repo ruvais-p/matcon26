589f532c842b93e402df807faf7dbedb5057fa20
ruvais <ruvaispuv@gmail.com>
2026-07-06 16:04:33 +0530

    table removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/Hero/Hero";
 import About from "@/components/About/About";
 import Countdown from "@/components/Countdown/Countdown";
 import Speakers from "@/components/Speakers/Speakers";
-import RegistrationFeeTable from "@/components/RegistrationFeeTable/RegistrationFeeTable";
 import Footer from "@/components/Footer/Footer";
 import MatrixRain from "@/components/MatrixRain";
 
@@ -19,10 +18,6 @@ export default function Home() {
         <About />
         <Countdown />
         <Speakers />
-        <section style={{ padding: "60px 24px", background: "var(--bg, #020e04)" }}>
-          <RegistrationFeeTable />
-        </section>
-        <Footer />
       </main>
     </>
   );
